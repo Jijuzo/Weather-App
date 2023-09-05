85cfda27eab5fa100fbad5b0c74eab119983716f
Ihor Marchuk <usdreldo@gmai.com>
2023-09-05 19:42:02 +0200

    comments removed

--- src/utils/setSearchHistory.jsx
+++ src/utils/setSearchHistory.jsx
@@ -5,7 +5,6 @@ export const setSearchHistoryFunc = (
 ) => {
   const cityIndex = searchHistory.indexOf(location);
   if (cityIndex !== -1) {
-    // If the city is in the search history, remove it from the current position
     searchHistory.splice(cityIndex, 1);
   }
   if (!searchHistory.includes(location)) {
