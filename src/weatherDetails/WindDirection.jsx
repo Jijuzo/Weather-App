b19185ac294abdca3a225c1fd1aea2d1cb02b3f3
Ihor Marchuk <usdreldo@gmai.com>
2023-09-11 13:58:42 +0200

    const moved out of function

--- src/weatherDetails/WindDirection.jsx
+++ src/weatherDetails/WindDirection.jsx
@@ -1,6 +1,25 @@
 import React from "react";
 import "./WindDirection.css";
 
+const directions = [
+  { range: [360, 21], name: "N" },
+  { range: [22, 44], name: "NNE" },
+  { range: [45, 66], name: "NE" },
+  { range: [67, 89], name: "ENE" },
+  { range: [90, 111], name: "E" },
+  { range: [112, 134], name: "ESE" },
+  { range: [135, 156], name: "SE" },
+  { range: [157, 179], name: "SSE" },
+  { range: [180, 201], name: "S" },
+  { range: [202, 224], name: "SSW" },
+  { range: [225, 246], name: "SW" },
+  { range: [247, 269], name: "WSW" },
+  { range: [270, 291], name: "W" },
+  { range: [292, 314], name: "WNW" },
+  { range: [315, 336], name: "NW" },
+  { range: [337, 359], name: "NNW" },
+];
+
 export function WindDirection({ currentWeather }) {
   return (
     <div className="highlight-additional">
@@ -24,25 +43,6 @@ export function WindDirection({ currentWeather }) {
 }
 
 function degreesToText(deg) {
-  const directions = [
-    { range: [360, 21], name: "N" },
-    { range: [22, 44], name: "NNE" },
-    { range: [45, 66], name: "NE" },
-    { range: [67, 89], name: "ENE" },
-    { range: [90, 111], name: "E" },
-    { range: [112, 134], name: "ESE" },
-    { range: [135, 156], name: "SE" },
-    { range: [157, 179], name: "SSE" },
-    { range: [180, 201], name: "S" },
-    { range: [202, 224], name: "SSW" },
-    { range: [225, 246], name: "SW" },
-    { range: [247, 269], name: "WSW" },
-    { range: [270, 291], name: "W" },
-    { range: [292, 314], name: "WNW" },
-    { range: [315, 336], name: "NW" },
-    { range: [337, 359], name: "NNW" },
-  ];
-
   const matchedDirection = directions.find(
     ({ range }) => deg >= range[0] && deg <= range[1]
   );
