05a7de4fdb414e18d2f1fd5c4685070985162bbb
Ihor Marchuk <usdreldo@gmai.com>
2023-09-11 14:02:33 +0200

    styles moved to css file

--- src/weatherDetails/ProgressBar.jsx
+++ src/weatherDetails/ProgressBar.jsx
@@ -4,14 +4,14 @@ export const ProgressBar = ({ value }) => {
   return (
     <div className="highlight-additional">
       <div className="slider-numbers">
-        <p style={{ margin: "0" }}>0</p>
-        <p style={{ margin: "0" }}>50</p>
-        <p style={{ margin: "0" }}>100</p>
+        <p className="slider-number">0</p>
+        <p className="slider-number">50</p>
+        <p className="slider-number">100</p>
       </div>
       <div className="slider">
         <div className="slider-value" style={{ width: `${value}%` }}></div>
       </div>
-      <p style={{ width: "70%", margin: "0", textAlign: "right" }}>%</p>
+      <p className="slider-percent-char">%</p>
     </div>
   );
 };
