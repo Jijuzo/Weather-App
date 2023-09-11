d5103cff97864c5b56f0bf47e7a34016bf3e8f24
Ihor Marchuk <usdreldo@gmai.com>
2023-09-11 14:05:45 +0200

    const moved out of function

--- src/weatherDetails/ForecastImage.jsx
+++ src/weatherDetails/ForecastImage.jsx
@@ -1,7 +1,8 @@
 import "./ForecastImage.css";
 
+const baseUrl = "https://openweathermap.org";
+
 export const ForecastImage = ({ icon, description }) => {
-  const baseUrl = "https://openweathermap.org";
   const imageUrl = new URL(`/img/wn/${icon}@4x.png`, baseUrl);
   return <img className="forecast-image" src={imageUrl} alt={description} />;
 };
