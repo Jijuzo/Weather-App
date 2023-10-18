d44162e55072eeeeb1910a5ff2b19c919fd09d9e
Ihor Marchuk <usdreldo@gmai.com>
2023-10-18 21:42:09 +0200

    .jsx and .js filed changed to .tsx and .ts

--- src/weatherDetails/ErrorAlert.jsx
+++
@@ -1,8 +0,0 @@
-import "./ErrorAlert.css";
-
-const errorAlertText =
-  "Something went wrong while downloading weather information. Please check your internet connection and try again.";
-
-export const ErrorAlert = ({ error }) => {
-  return error ? <div className="error-alert">{errorAlertText}</div> : null;
-};
