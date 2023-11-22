682c783fe249497a1dcb3b4b87e99a2b1c93a4d4
Ihor Marchuk <usdreldo@gmai.com>
2023-11-22 17:55:25 +0100

    type changed

--- src/weatherDetails/ErrorAlert.tsx
+++ src/weatherDetails/ErrorAlert.tsx
@@ -3,6 +3,6 @@ import "./ErrorAlert.css";
 const errorAlertText =
   "Something went wrong while downloading weather information. Please check your internet connection and try again.";
 
-export const ErrorAlert = ({ error }: { error: string | null }) => {
+export const ErrorAlert = ({ error }: { error: Error | null }) => {
   return error ? <div className="error-alert">{errorAlertText}</div> : null;
 };
